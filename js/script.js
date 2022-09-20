397c10821f362686ed6879b426ec10adb9b0d62c
felipeluis223 <lf9072962@gmail.com>
2022-09-20 20:41:42 -0300

    conteúdo da home e carrosel de imagens

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ menuBtn.onclick = ()=>{
 	menuBtn.classList.toggle("fa-times");
 	navbar.classList.toggle("active");
 }
+
+var swiper = new Swiper(".course-slider",{
+	spaceBetween: 20,
+	grabCursor: true,
+	loop: true,
+	pagination: {
+		el:".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints:{
+		540:{
+			slidePerView: 1,
+		},
+		768:{
+			slidePerView: 2,
+		},
+		1024:{
+			slidePerView: 3,
+		}
+	}
+});
